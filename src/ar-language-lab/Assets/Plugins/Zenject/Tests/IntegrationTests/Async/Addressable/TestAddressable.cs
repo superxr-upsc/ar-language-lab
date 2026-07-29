1e7bf3508b4d38261d193af6b7a67a9f76a54666
NobodyI <igorash177@gmail.com>
2026-07-29 14:08:16 +0300

    added localisation package

--- src/ar-language-lab/Assets/Plugins/Zenject/Tests/IntegrationTests/Async/Addressable/TestAddressable.cs
+++ src/ar-language-lab/Assets/Plugins/Zenject/Tests/IntegrationTests/Async/Addressable/TestAddressable.cs
@@ -38,7 +38,7 @@ public class TestAddressable : ZenjectIntegrationTestFixture
                 Assert.Greater(locationsHandle.Result.Count, 0, "Key required for test is not configured. Check Readme.txt in addressable test folder");
 
                 IResourceLocation location = locationsHandle.Result[0];
-                handle = Addressables.LoadAsset<GameObject>(location);
+                handle = Addressables. LoadAssetAsync<GameObject>(location);
                 await handle.Task;
                 return handle.Result;
             }
